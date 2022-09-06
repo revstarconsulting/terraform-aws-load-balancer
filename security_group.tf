dfe4241d9a5dd5c3a696c0cc0f4c2dd208df4750
ricky <ricky@cloudprodigy.ca>
2022-09-06 12:26:32 -0400

    alb config

---
+++ security_group.tf
@@ -0,0 +1,28 @@
+resource "aws_security_group" "load_balancer" {
+  count       = var.create_alb ? 1 : 0
+  name        = "${var.lb_name}_sg"
+  description = "Controls access to the ALB"
+  tags        = local.common_tags
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] #tfsec:ignore:AWS008
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] #tfsec:ignore:AWS008
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"] #tfsec:ignore:AWS009
+  }
+}
